7450548e9b3e74a85beb1fed7373a310f3b7522d
leehh <newlhh2@naver.com>
2020-08-27 17:09:20 +0900

    200827

--- public/js/user.js
+++ public/js/user.js
@@ -38,6 +38,39 @@ exports.login = function (request, response) {
 
 }
 
+exports.kakaoLogin = function (request, response) {
+  var user_id = request.body.username;
+  var password = request.body.password;
+  db.query('select * from user_info where user_id = ?', [user_id], function (err, result) {
+      if (err) {
+        console.log('err :' + err);
+      } else {
+          if (result.length === 0) {
+	    console.log("id 없음.");
+            response.json({success: false, msg: '존재하지 않습니다.'})
+          } else {
+	      bcrypt.compare(password, result[0].password, function(err, res) {
+		if (res) { // 비교 성공
+		 request.session.user_id = result[0].user_id;
+		 request.session.user_name = result[0].user_name;
+		 request.session.user_tell = result[0].user_tell;
+		 request.session.isLogined = true;
+		 console.log("login success, user_id : " + request.session.user_id);
+                 request.session.save(function(){
+		   response.redirect('/');
+		 });
+		 //response.render('index', {session : request.session});
+                } else { // 비교 실패
+		    console.log("password incorrected");
+		    response.redirect('/');
+		  }
+});
+          }
+        }
+      });
+
+}
+
 exports.signup = function ( request, response ){
   var user = request.body;
 
