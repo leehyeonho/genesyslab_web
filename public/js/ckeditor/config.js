6fec69a4fe5f480eb9e494a1fb661ef8281fce5d
leehh <newlhh2@naver.com>
2020-09-29 14:01:23 +0900

    200929

--- public/js/ckeditor/config.js
+++ public/js/ckeditor/config.js
@@ -7,6 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
 	// For complete reference see:
 	// https://ckeditor.com/docs/ckeditor4/latest/api/CKEDITOR_config.html
+  config.enterMode = CKEDITOR.ENTER_BR;
+  config.shiftEnterMode = CKEDITOR.ENTER_P;
 
 	// The toolbar groups arrangement, optimized for two toolbar rows.
 	config.toolbarGroups = [
