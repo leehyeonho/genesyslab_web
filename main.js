78a068394fe4a12fe632f88ba7d780db7951a665
leehh <newlhh2@naver.com>
2020-10-24 16:50:13 +0900

    201024

--- main.js
+++ main.js
@@ -99,33 +99,33 @@ var sub = require('./public/js/sub.js');
 app.set('view engine', 'ejs');
 app.set('views', './public/views');
 
-const mime = require('mime');
-
-var getDownloadFilename = require('./library/getDownloadFilename').getDownloadFilename;
-
-app.get('/images/:file_name', function(req, res, next) {
-    var upload_folder = '/images/';
-    var file = upload_folder + req.params.file_name;
-    try {
-    if (fs.existsSync(file)) { // 파일이 존재하는지 체크
-      var filename = path.basename(file); // 파일 경로에서 파일명(확장자포함)만 추출
-      var mimetype = mime.getType(file); // 파일의 타입(형식)을 가져옴
-
-      res.setHeader('Content-disposition', 'attachment; filename=' + getDownloadFilename(req, filename)); // 다운받아질 파일명 설정
-      res.setHeader('Content-type', mimetype); // 파일 형식 지정
-
-      var filestream = fs.createReadStream(file);
-      filestream.pipe(res);
-    } else {
-      res.send('해당 파일이 없습니다.');
-      return;
-    }
-  } catch (e) { // 에러 발생시
-    console.log(e);
-    res.send('파일을 다운로드하는 중에 에러가 발생하였습니다.');
-    return;
-  }
-})
+// const mime = require('mime');
+//
+// var getDownloadFilename = require('./library/getDownloadFilename').getDownloadFilename;
+//
+// app.get('/images/:file_name', function(req, res, next) {
+//     var upload_folder = '/images/';
+//     var file = upload_folder + req.params.file_name;
+//     try {
+//     if (fs.existsSync(file)) { // 파일이 존재하는지 체크
+//       var filename = path.basename(file); // 파일 경로에서 파일명(확장자포함)만 추출
+//       var mimetype = mime.getType(file); // 파일의 타입(형식)을 가져옴
+//
+//       res.setHeader('Content-disposition', 'attachment; filename=' + getDownloadFilename(req, filename)); // 다운받아질 파일명 설정
+//       res.setHeader('Content-type', mimetype); // 파일 형식 지정
+//
+//       var filestream = fs.createReadStream(file);
+//       filestream.pipe(res);
+//     } else {
+//       res.send('해당 파일이 없습니다.');
+//       return;
+//     }
+//   } catch (e) { // 에러 발생시
+//     console.log(e);
+//     res.send('파일을 다운로드하는 중에 에러가 발생하였습니다.');
+//     return;
+//   }
+// })
 
 
 
