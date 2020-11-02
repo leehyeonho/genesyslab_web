3a4177ca64bb2699081a1a0a652d48679e91cc82
leehh <newlhh2@naver.com>
2020-11-02 09:58:00 +0900

    201102

--- public/js/connection.js
+++ public/js/connection.js
@@ -1,13 +1,5 @@
 var mysql = require('mysql');
 
-// var connection = mysql.createConnection({
-//   host      : 'localhost',
-//   port:3306,
-//   user      : 'root',
-//   password  : 'customlab11',
-//   database  : 'customlab'
-// });
-
 var connection = mysql.createConnection({
   host      : 'localhost',
   port:3306,
